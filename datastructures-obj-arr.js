3cb36d7ac9e6c9344c72e3d1a7d078dd5a48ef63
Kamoliddin <ulbarsovk@gmail.com>
2024-12-03 03:21:44 +0900

    Done with the first exercise, sealed with a comment!

--- datastructures-obj-arr.js
+++ datastructures-obj-arr.js
@@ -1,19 +1,19 @@
-    // EXERCIE 1: the sum of a range
+// EXERCIE 1: the sum of a range
 
-function range(start, end, step=1) {
-    result = [];
-    for (let counter = start; start < end ? counter <= end : counter >= end; counter += Math.round(step)) {
-        result.push(counter);
-    };
-    return result;
-};
+// function range(start, end, step=1) {
+//     result = [];
+//     for (let counter = start; start < end ? counter <= end : counter >= end; counter += Math.round(step)) {
+//         result.push(counter);
+//     };
+//     return result;
+// };
 
-function sum(arr) {
-    result = 0;
-    for (let num of arr) {
-        result += num;
-    };
-    return result;
-};
+// function sum(arr) {
+//     result = 0;
+//     for (let num of arr) {
+//         result += num;
+//     };
+//     return result;
+// };
 
-console.log(range(10, 1, -1))
+// console.log(range(10, 1, -1))
