5e3c91ba2f968066342d64c78d6888a716e63cda
Kamoliddin <ulbarsovk@gmail.com>
2024-12-05 00:45:30 +0900

    Added the summary of the higher order functions chapter from EJS describing the various higher order array methods.

---
+++ higher-order-functions.js
@@ -0,0 +1,8 @@
+// Arrays provide a number of useful higher-order methods. 
+// You can use forEach to loop over the elements in an array. 
+// The filter method returns a new array containing only the elements that pass the predicate function. 
+// You can transform an array by putting each element through a function using map. 
+// You can use reduce to combine all the elements in an array into a single value. 
+// The some method tests whether any element matches a given predicate function, while find finds the first element that matches a predicate.
+
+
