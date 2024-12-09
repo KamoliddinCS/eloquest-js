d79e92f2708000f5ae6e999767afc49f3056dccf
Kamoliddin <ulbarsovk@gmail.com>
2024-12-09 20:10:02 +0900

    Playing around with the DOM API

--- main.js
+++ main.js
@@ -42,4 +42,58 @@
 
 // changing the title of an html document using js
 
-let heading = document.getElementById("main-heading");
+// let heading = document.querySelector("#main-heading");
+
+// heading.addEventListener("click", () => {
+//     console.log("You clicked the heading");
+// });
+
+// // document.querySelector(".highlight").addEventListener("click", () => { console.log("Clicked on the <em> tag") });
+// // document.querySelector("body").addEventListener("click", () => { console.log("Clicked on the body tag.") });
+
+// function updateElement(element, desiredText=null) {
+//     element.textContent = desiredText;
+// }
+
+// document.querySelector(".important").addEventListener("click", () => { updateElement(heading, "H2 with important class was clicked!!!") })
+
+let wordlist = document.querySelector("#word-list");
+let sentence = document.querySelector("#sentence");
+
+wordlist.addEventListener("click", event => {
+    let word = event.target.textContent;
+    sentence.textContent += word;
+    sentence.textContent += " ";
+});
+
+let box = document.querySelector("#box");
+
+// document.querySelector("html").addEventListener("mousemove", 
+//     e => {
+//         box.style.display = 'block';
+//         box.style.position = 'fixed';
+//         box.style.left = e.clientX + 10 + 'px';
+//         box.style.top = e.clientY - 15 + 'px';
+// });
+
+let currentX = 0;
+let currentY = 0;
+
+document.querySelector("html").addEventListener("keydown", e => {
+    if (e.repeat == false) {
+        if (e.key == "w") {
+            currentY -= 20;
+        } else if (e.key == "a") {
+            currentX -= 20;
+        } else if (e.key == "s") {
+            currentY += 20;
+        } else if (e.key == "d") {
+            currentX += 20;
+        }
+    };
+
+    box.style.display = 'block';
+    box.style.position = 'fixed';
+    box.style.left = currentX + 'px';
+    box.style.top = currentY + 'px';
+});
